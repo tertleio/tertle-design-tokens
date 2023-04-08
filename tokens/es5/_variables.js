c991c05d5ee9037db255570ef721464cde58903c
Ryan Connaughton <hello@ryanconnaughton.com>
2023-04-08 14:23:02 +0100

    rebuild

---
+++ tokens/es5/_variables.js
@@ -0,0 +1,416 @@
+/**
+ * Do not edit directly
+ * Generated on Sat, 08 Apr 2023 13:21:44 GMT
+ */
+
+module.exports = {
+  "global": {
+    "lightest": {
+      "value": "#FFFFFF",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#FFFFFF"
+      },
+      "name": "GlobalLightest",
+      "attributes": {
+        "category": "global",
+        "type": "lightest"
+      },
+      "path": [
+        "global",
+        "lightest"
+      ]
+    },
+    "darkest": {
+      "value": "#000000",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#000000"
+      },
+      "name": "GlobalDarkest",
+      "attributes": {
+        "category": "global",
+        "type": "darkest"
+      },
+      "path": [
+        "global",
+        "darkest"
+      ]
+    }
+  },
+  "color": {
+    "base": {
+      "value": "#ffffff",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#FFFFFF"
+      },
+      "name": "ColorBase",
+      "attributes": {
+        "category": "color",
+        "type": "base"
+      },
+      "path": [
+        "color",
+        "base"
+      ]
+    },
+    "bg": {
+      "value": "#f6f8fa",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#F6F8FA"
+      },
+      "name": "ColorBg",
+      "attributes": {
+        "category": "color",
+        "type": "bg"
+      },
+      "path": [
+        "color",
+        "bg"
+      ]
+    },
+    "fg": {
+      "value": "#ffffff",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#ffffff"
+      },
+      "name": "ColorFg",
+      "attributes": {
+        "category": "color",
+        "type": "fg"
+      },
+      "path": [
+        "color",
+        "fg"
+      ]
+    },
+    "primary": {
+      "value": "#60964f",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#60964f"
+      },
+      "name": "ColorPrimary",
+      "attributes": {
+        "category": "color",
+        "type": "primary"
+      },
+      "path": [
+        "color",
+        "primary"
+      ]
+    },
+    "seconary": {
+      "value": "#000000",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#000000"
+      },
+      "name": "ColorSeconary",
+      "attributes": {
+        "category": "color",
+        "type": "seconary"
+      },
+      "path": [
+        "color",
+        "seconary"
+      ]
+    },
+    "shadow1": {
+      "value": "#eeeeee",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#EEEEEE"
+      },
+      "name": "ColorShadow1",
+      "attributes": {
+        "category": "color",
+        "type": "shadow1"
+      },
+      "path": [
+        "color",
+        "shadow1"
+      ]
+    },
+    "gray100": {
+      "value": "#eeeeee",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#EEEEEE"
+      },
+      "name": "ColorGray100",
+      "attributes": {
+        "category": "color",
+        "type": "gray100"
+      },
+      "path": [
+        "color",
+        "gray100"
+      ]
+    },
+    "gray200": {
+      "value": "#e5e5e5",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#E5E5E5"
+      },
+      "name": "ColorGray200",
+      "attributes": {
+        "category": "color",
+        "type": "gray200"
+      },
+      "path": [
+        "color",
+        "gray200"
+      ]
+    },
+    "gray400": {
+      "value": "#cccccc",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#CCCCCC"
+      },
+      "name": "ColorGray400",
+      "attributes": {
+        "category": "color",
+        "type": "gray400"
+      },
+      "path": [
+        "color",
+        "gray400"
+      ]
+    },
+    "gray600": {
+      "value": "#999999",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#999999"
+      },
+      "name": "ColorGray600",
+      "attributes": {
+        "category": "color",
+        "type": "gray600"
+      },
+      "path": [
+        "color",
+        "gray600"
+      ]
+    },
+    "gray800": {
+      "value": "#6f6f6f",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#6F6F6F"
+      },
+      "name": "ColorGray800",
+      "attributes": {
+        "category": "color",
+        "type": "gray800"
+      },
+      "path": [
+        "color",
+        "gray800"
+      ]
+    },
+    "gray900": {
+      "value": "#313131",
+      "filePath": "tokens/json/colors-light.json",
+      "isSource": true,
+      "original": {
+        "value": "#313131"
+      },
+      "name": "ColorGray900",
+      "attributes": {
+        "category": "color",
+        "type": "gray900"
+      },
+      "path": [
+        "color",
+        "gray900"
+      ]
+    }
+  },
+  "lightest": {
+    "value": "#FFFFFF",
+    "filePath": "tokens/json/colors-global.json",
+    "isSource": true,
+    "original": {
+      "value": "#FFFFFF"
+    },
+    "name": "Lightest",
+    "attributes": {
+      "category": "lightest"
+    },
+    "path": [
+      "lightest"
+    ]
+  },
+  "darkest": {
+    "value": "#000000",
+    "filePath": "tokens/json/colors-global.json",
+    "isSource": true,
+    "original": {
+      "value": "#000000"
+    },
+    "name": "Darkest",
+    "attributes": {
+      "category": "darkest"
+    },
+    "path": [
+      "darkest"
+    ]
+  },
+  "font": {
+    "size": {
+      "xxsm": {
+        "value": "12px",
+        "filePath": "tokens/json/typography.json",
+        "isSource": true,
+        "original": {
+          "value": "12px"
+        },
+        "name": "FontSizeXxsm",
+        "attributes": {
+          "category": "font",
+          "type": "size",
+          "item": "xxsm"
+        },
+        "path": [
+          "font",
+          "size",
+          "xxsm"
+        ]
+      },
+      "xsm": {
+        "value": "14px",
+        "filePath": "tokens/json/typography.json",
+        "isSource": true,
+        "original": {
+          "value": "14px"
+        },
+        "name": "FontSizeXsm",
+        "attributes": {
+          "category": "font",
+          "type": "size",
+          "item": "xsm"
+        },
+        "path": [
+          "font",
+          "size",
+          "xsm"
+        ]
+      },
+      "sm": {
+        "value": "16px",
+        "filePath": "tokens/json/typography.json",
+        "isSource": true,
+        "original": {
+          "value": "16px"
+        },
+        "name": "FontSizeSm",
+        "attributes": {
+          "category": "font",
+          "type": "size",
+          "item": "sm"
+        },
+        "path": [
+          "font",
+          "size",
+          "sm"
+        ]
+      },
+      "md": {
+        "value": "24px",
+        "filePath": "tokens/json/typography.json",
+        "isSource": true,
+        "original": {
+          "value": "24px"
+        },
+        "name": "FontSizeMd",
+        "attributes": {
+          "category": "font",
+          "type": "size",
+          "item": "md"
+        },
+        "path": [
+          "font",
+          "size",
+          "md"
+        ]
+      },
+      "lg": {
+        "value": "38px",
+        "filePath": "tokens/json/typography.json",
+        "isSource": true,
+        "original": {
+          "value": "38px"
+        },
+        "name": "FontSizeLg",
+        "attributes": {
+          "category": "font",
+          "type": "size",
+          "item": "lg"
+        },
+        "path": [
+          "font",
+          "size",
+          "lg"
+        ]
+      },
+      "xlg": {
+        "value": "50px",
+        "filePath": "tokens/json/typography.json",
+        "isSource": true,
+        "original": {
+          "value": "50px"
+        },
+        "name": "FontSizeXlg",
+        "attributes": {
+          "category": "font",
+          "type": "size",
+          "item": "xlg"
+        },
+        "path": [
+          "font",
+          "size",
+          "xlg"
+        ]
+      },
+      "xxlg": {
+        "value": "62px",
+        "filePath": "tokens/json/typography.json",
+        "isSource": true,
+        "original": {
+          "value": "62px"
+        },
+        "name": "FontSizeXxlg",
+        "attributes": {
+          "category": "font",
+          "type": "size",
+          "item": "xxlg"
+        },
+        "path": [
+          "font",
+          "size",
+          "xxlg"
+        ]
+      }
+    }
+  }
+};
